01397b82700d32c5f18f48d6ea5e4d4f31dbfa8e
AnberYuan <yuan.linghui@towngas.com.cn>
2020-12-16 09:48:36 +0800

    example

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import App from './pages/hello';
+
+new Vue({
+  el: '#app',
+  components: {
+    App
+  },
+  template: '<App />'
+});
+// function show(){
+//   return 'hello anber'
+// }
+// console.log(show());
---
+++ src/pages/hello.vue
@@ -0,0 +1,14 @@
+<template>
+   <div>
+     <h1>Hello World</h1>
+     <div>
+       <img src="../assets/lz.jpg" alt="">
+     </div>
+   </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
